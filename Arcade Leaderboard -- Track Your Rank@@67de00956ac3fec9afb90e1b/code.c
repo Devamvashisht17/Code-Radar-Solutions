5984b24ede7207fe9b9bf03668e717adfe648028
Devamvashisht17 <devamvashisht1704@gmail.com>
2025-03-29 18:06:48 +0530

    Solution to Arcade Leaderboard -- Track Your Rank

--- Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
+++ Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
@@ -1,24 +1,42 @@
-def get_dense_ranks(leaderboard_scores):
-    dense_ranks = []
-    rank = 1
-    prev_score = None
-    
-    for score in leaderboard_scores:
-        if score != prev_score:
-            rank = len(dense_ranks) + 1
-        dense_ranks.append(rank)
-        prev_score = score
-    
-    return dense_ranks
+#include <stdio.h>
 
-def get_player_ranks(leaderboard_scores, player_scores):
-    unique_scores = sorted(set(leaderboard_scores), reverse=True)
-    ranks = []
-    index = len(unique_scores)  # Start from the lowest rank
+void get_player_ranks(int leaderboard[], int n, int player_scores[], int m) {
+    int unique_scores[n];
+    int rank[n];
+    int count = 0;
+
+    // Creating unique scores with dense ranks
+    for (int i = 0; i < n; i++) {
+        if (i == 0 || leaderboard[i] != leaderboard[i - 1]) {
+            unique_scores[count] = leaderboard[i];
+            rank[count] = count + 1;
+            count++;
+        }
+    }
+
+    int index = count; // Start from the lowest rank
+    for (int i = 0; i < m; i++) {
+        while (index > 0 && player_scores[i] >= unique_scores[index - 1]) {
+            index--;
+        }
+        printf("%d\n", index + 1);
+    }
+}
+
+int main() {
+    int n, m;
+    scanf("%d", &n);
+    int leaderboard[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &leaderboard[i]);
+    }
     
-    for score in player_scores:
-        while index > 0 and score >= unique_scores[index - 1]:
-            index -= 1
-        ranks.append(index + 1)
+    scanf("%d", &m);
+    int player_scores[m];
+    for (int i = 0; i < m; i++) {
+        scanf("%d", &player_scores[i]);
+    }
     
-    return ranks
+    get_player_ranks(leaderboard, n, player_scores, m);
+    return 0;
+}
